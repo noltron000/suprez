52b516ff6efee78150b866428fc9bce8520e79a4
sadboyKJ <yourboykjdev@gmail.com>
2019-02-17 22:39:15 -0800

    search term query being constructed :construction:

--- routes/giph.js
+++ routes/giph.js
@@ -5,13 +5,24 @@ const auth = require('./helpers/auth');
 
 // GET Giphs
 router.get('/hello-gif', function (req, res) {
-    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-    res.render('hello-gif', {gifUrl: gifUrl})
+    res.render('gif-home', {});
 })
 
-router.get('/greetings/:name', function (req, res) {
-    var name = req.params.name;
-    res.render('greetings',  {name:name} );
-})
+// GET Giphs
+// router.get('/hello-gif', function (req, res) {
+//     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
+//     res.render('hello-gif', {gifUrl: gifUrl});
+// })
+
+// GET Greeting Page
+// router.get('/greetings/:name', function (req, res) {
+//     var name = req.params.name;
+//     res.render('greetings',  {name: name} );
+// })
+
+// GET Home Page for Giphs
+// router.get('/home', function (req, res) {
+//     res.render('gif-home', {});
+// })
 
 module.exports = router;
