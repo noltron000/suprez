6582845f17ce8a1b0872fec0de7997760a8fc2f3
sadboyKJ <yourboykjdev@gmail.com>
2019-02-19 00:58:59 -0800

    gify search bar synced to API :dizzy_face:

--- routes/giph.js
+++ routes/giph.js
@@ -3,10 +3,46 @@ const router = express.Router();
 const User = require('../models/user');
 const auth = require('./helpers/auth');
 
-// GET Giphs
-router.get('/hello-gif', function (req, res) {
+var http = require('http');
+
+// GIPHY API
+router.get('/', function (req, res) {
+    var queryString = req.query.term;
+    console.log(queryString);
+    // removes white spaces and restricted characters
+    var term = encodeURIComponent(queryString);
+    // putting the search term into GIPHY API
+    var url  = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=M6qnJc0px1XEaQH1IXI0eQaa6dQWi886'
+
+    http.get(url, function(response) {
+
+        // sets response to utf8
+        response.setEncoding('utf8');
+
+        var body = '';
+
+        response.on('data', function(d) {
+            // continuously updates stream with data from giphy
+            body += d;
+        });
+
+        response.on('end', function() {
+            // retrieves finished data and parses it (JSON)
+            var parse = JSON.parse(body);
+            // renders the home template and pass gif data to template
+            res.render('gif-home', {gifs: parsed.data})
+        });
+
+    });
+})
+
+// GET Giphs Search
+router.get('/', function (req, res) {
+    console.log(req.query);
     res.render('gif-home', {});
 })
+
+
 
 // GET Giphs
 // router.get('/hello-gif', function (req, res) {
